58349a9d70cf7d9751f404ec7f93d1d0411c1517
Thomas Shih <tshih17110@gmail.com>
2023-02-14 17:02:52 +0800

    Added breakpoints

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,24 +7,19 @@ const Footer = () => {
     return (
         <footer className='footer'>
             <div className="footer-container">
-                <div className="logo-links">
+                <div className='logo-links'>
                     <a className='github-link' href='https://github.com/tshih17110' target='_blank'>
-                        <IconContext.Provider value={{size: '36px', className: 'github-logo'}}>
+                        <IconContext.Provider value={{size: 'auto', className: 'github-logo'}}>
                             <FaGithub />
                         </IconContext.Provider>
                     </a>
                     <a className='linkedin-link' href='https://linkedin.com/in/thomas-shih-105674184' target='_blank'>
-                        <IconContext.Provider value={{size: '36px',className: 'linkedin-logo'}}>
+                        <IconContext.Provider value={{size: 'auto',className: 'linkedin-logo'}}>
                             <FaLinkedin />
                         </IconContext.Provider>
                     </a>                    
                 </div>
-
-                
-
-
-
-                <p>Built by Thomas Shih | 2023</p>    
+                <p className='footer-description'>Built by Thomas Shih | 2023</p>    
             </div>
         </footer>
    
